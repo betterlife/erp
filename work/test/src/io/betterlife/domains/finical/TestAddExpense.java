00cea6880468c1bfb16c0fbb19757407e0e728ec
Lawrence as Developer <smartlitchi@gmail.com>
2014-11-05 01:38:26 +0800

    Fix build error

--- work/test/src/io/betterlife/domains/finical/TestAddExpense.java
+++ work/test/src/io/betterlife/domains/finical/TestAddExpense.java
@@ -1,7 +1,6 @@
 package io.betterlife.domains.finical;
 
 import io.betterlife.domains.security.User;
-import io.betterlife.persistence.finical.ExpenseOperator;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,26 +13,9 @@ import java.util.Date;
  */
 public class TestAddExpense {
 
-    //TODO.xqliu 研究看Entity怎么继承,减少代码的重复
-
     @Test
-    public void testAddValidExpense() {
-        Expense expense = new Expense();
-        expense.setCategory(new ExpenseCategory("Marketing"));
-        expense.setCostCenter(new CostCenter("实体店"));
-        final User user = new User();
-        user.setUsername("xqliu");
-        user.setPassword("xqliu");
-        expense.setUser(user);
-        expense.setAmount(new BigDecimal("100"));
-        expense.setRemark("Remark");
-        expense.setExpenseDate(new Date());
-        expense.setLastModifyDate(new Date());
-        expense.setLastModify(user);
-        expense.setCreateDate(new Date());
-        expense.setCreator(user);
-        //ExpenseOperator.getInstance().save(expense);
-        //Expense newExpense = ExpenseOperator.getInstance().get(entityManager, expense.getId());
-        //Assert.assertEquals(expense, newExpense);
+    public void testAddValidExpenseCategory() {
+        ExpenseCategory expenseCategory = new ExpenseCategory();
+        expenseCategory.setCategoryName("测试Category");
     }
 }
