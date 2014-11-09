bbaf3e88f1a4ac6e2cd2e5a919672088dfa0892a
Lawrence as Developer <smartlitchi@gmail.com>
2014-11-10 00:22:59 +0800

    Fixes #16, Configure log4j, currently log4j2 is used.

--- work/src/io/betterlife/rest/security/SecurityService.java
+++ work/src/io/betterlife/rest/security/SecurityService.java
@@ -4,18 +4,22 @@ import io.betterlife.application.ApplicationConfig;
 import io.betterlife.domains.security.User;
 import io.betterlife.persistence.BaseOperator;
 import io.betterlife.util.rest.ExecuteResult;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.ws.rs.*;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Author: Lawrence Liu(lawrence@betterlife.io)
@@ -29,12 +33,15 @@ public class SecurityService {
     @PersistenceContext(unitName = ApplicationConfig.PersistenceUnitName)
     private EntityManager entityManager;
 
-    private static final Logger logger = Logger.getLogger(SecurityService.class.getName());
+    private static final Logger logger = LogManager.getLogger(SecurityService.class.getName());
 
     @POST
     @Path("/logout/{username}")
     @Produces(MediaType.APPLICATION_JSON)
     public String logout(@PathParam("username") String username) throws IOException {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Logout user: " + username);
+        }
         return ExecuteResult.getRestString("LOGOUT");
     }
 
@@ -45,16 +52,18 @@ public class SecurityService {
                         @PathParam("password") String password) throws IOException {
         User user = null;
         try {
+            if (logger.isDebugEnabled()) {
+                logger.debug(String.format("Login request, [%s:%s]", username, password));
+            }
             Map<String, String> params = new HashMap<>(2);
             params.put("username", username);
             params.put("password", password);
             user = BaseOperator.getInstance().getBaseObject(entityManager, User.GetByUserNameAndPasswordQuery, params);
         } catch (Exception e) {
-            logger.log(
-                Level.WARNING, String.format(
-                    "Error to get user username[%s%], password[%s%]",
-                    username, "HIDDEN_FOR_SECURITY"
-                )
+            logger.warn(String.format(
+                            "Error to get user username[%s%], password[%s%]",
+                            username, "HIDDEN_FOR_SECURITY"
+                        )
             );
         }
         return ExecuteResult.getRestString(user);
