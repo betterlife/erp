afb228edbf3006277a21e2682e3edf57d35d1e95
Lawrence as Developer <smartlitchi@gmail.com>
2014-11-10 23:40:08 +0800

    UI improvement(at least we think it is!)

--- work/WebContent/js/app/config.js
+++ work/WebContent/js/app/config.js
@@ -8,10 +8,19 @@
                 return $sce.trustAsHtml(text);
             };
         }])
-        .controller('mainController', function($scope, $route, $routeParams, $location) {
+        .controller('mainController', function($scope, $rootScope, $http, $location,
+                                               loginService, $route, $routeParams) {
             $scope.$route = $route;
             $scope.$location = $location;
             $scope.$routeParams = $routeParams;
+
+            $scope.isActive = function (route) {
+                return route === $location.path();
+            };
+
+            $scope.isLoggedIn = function (){
+                return loginService.isLoggedIn();
+            }
         })
         .config(['$routeProvider', '$locationProvider',
             function ($routeProvider, $locationProvider) {
@@ -28,9 +37,8 @@
             }])
         .run(['$rootScope', '$location', 'loginService',
             function ($rootScope, $location, loginService) {
-                // Redirect to login if route requires auth and you're not logged in
-                console.log("Register the routeChange start event");
-                console.log("current $location.path is: %s", $location.path());
+                console.info("Register the routeChange start event");
+                console.debug("current $location.path is: %s", $location.path());
                 $rootScope.$on('$routeChangeStart', function (event, next) {
                     console.log("Checking for login: %s", loginService.isLoggedIn());
                     if (next.authenticate && !loginService.isLoggedIn()) {
