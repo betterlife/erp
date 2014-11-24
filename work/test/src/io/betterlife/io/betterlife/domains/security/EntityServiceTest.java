03f439937b498f060cadc8cef5e5788802a3246c
Lawrence as Developer <smartlitchi@gmail.com>
2014-11-24 18:09:52 +0800

    Remove unused openJPAUtil from EntityService

--- work/test/src/io/betterlife/io/betterlife/domains/security/EntityServiceTest.java
+++ work/test/src/io/betterlife/io/betterlife/domains/security/EntityServiceTest.java
@@ -3,6 +3,7 @@ package io.betterlife.io.betterlife.domains.security;
 import io.betterlife.domains.security.User;
 import io.betterlife.persistence.BaseOperator;
 import io.betterlife.rest.EntityService;
+import io.betterlife.util.EntityMockUtil;
 import io.betterlife.util.JsonUtils;
 import io.betterlife.util.jpa.OpenJPAUtil;
 import io.betterlife.util.rest.ExecuteResult;
@@ -14,19 +15,13 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import javax.persistence.EntityManager;
-import javax.persistence.metamodel.Attribute;
-import javax.persistence.metamodel.ManagedType;
-import javax.persistence.metamodel.Metamodel;
 import javax.servlet.http.HttpServletRequest;
-
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
 
 /**
@@ -57,11 +52,10 @@ public class EntityServiceTest {
 
     @Test
     public void testGetUser() throws IOException {
-        when(operator.getBaseObjectById(entityManager, openJPAUtil, 1, "User.getById")).thenReturn(existingUser);
+        when(operator.getBaseObjectById(entityManager, 1, "User.getById")).thenReturn(existingUser);
         when(openJPAUtil.getOpenJPAQuery(entityManager, "User.getById")).thenReturn(openJPAQuery);
         EntityService entityService = new EntityService();
         entityService.setEntityManager(entityManager);
-        entityService.setOpenJPAUtil(openJPAUtil);
         entityService.setOperator(operator);
         final String userFromDB = entityService.getObjectByTypeAndId(1, "User");
         final JsonNode node = JsonUtils.getInstance().stringToJsonNode(userFromDB);
@@ -69,7 +63,7 @@ public class EntityServiceTest {
         assertEquals("username", username);
         final String password = node.get("result").get("password").getTextValue();
         assertEquals("password", password);
-        verify(operator, times(1)).getBaseObjectById(entityManager, openJPAUtil, 1, "User.getById");
+        verify(operator, times(1)).getBaseObjectById(entityManager, 1, "User.getById");
     }
 
     @Test
@@ -78,7 +72,7 @@ public class EntityServiceTest {
         EntityService entityService = new EntityService();
         entityService.setOperator(operator);
         HttpServletRequest servletRequest = mock(HttpServletRequest.class);
-        EntityManager manager = mockObjectsForEntityService(entityService);
+        EntityManager manager = EntityMockUtil.getInstance().mockObjectsForEntityService(entityService);
         Mockito.doNothing().when(manager).persist(newUser);
         Map<String, String> userMap = new HashMap<>(2);
         userMap.put("username", "xqliu");
@@ -91,55 +85,12 @@ public class EntityServiceTest {
     @Test
     public void testGetServiceEntity() throws IOException {
         EntityService service = new EntityService();
-        EntityManager manager = mockObjectsForEntityService(service);
+        EntityManager manager = EntityMockUtil.getInstance().mockObjectsForEntityService(service);
         String userMeta = service.getEntityMeta("User");
         JsonNode expect = JsonUtils.getInstance().stringToJsonNode("{\"result\":{\"id\":\"java.lang.Long\",\"username\":\"java.lang.String\",\"password\":\"java.lang.String\"},\"success\":true,\"successMessage\":null,\"errorMessages\":[]}\n");
         JsonNode node = JsonUtils.getInstance().stringToJsonNode(userMeta);
         assertEquals(expect, node);
     }
 
-    private EntityManager mockObjectsForEntityService(EntityService service) {
-        Set<Attribute> attributes = mockAttributes();
-        ManagedType type = mockManagedType(attributes);
-        Metamodel model = mockMetamodel(type);
-        EntityManager manager = mockEntityManager(model);
-        service.setEntityManager(manager);
-        return manager;
-    }
 
-    private EntityManager mockEntityManager(Metamodel model) {
-        EntityManager entityManager = mock(EntityManager.class);
-        when(entityManager.getMetamodel()).thenReturn(model);
-        return entityManager;
-    }
-
-    private Metamodel mockMetamodel(ManagedType type) {
-        Metamodel model = mock(Metamodel.class);
-        Set<ManagedType<?>> types = new HashSet<>();
-        types.add(type);
-        when(model.getManagedTypes()).thenReturn(types);
-        return model;
-    }
-
-    private ManagedType mockManagedType(Set<Attribute> attributes) {
-        ManagedType type = mock(ManagedType.class);
-        when(type.getJavaType()).thenReturn(User.class);
-        when(type.getAttributes()).thenReturn(attributes);
-        return type;
-    }
-
-    private Set<Attribute> mockAttributes() {
-        Set<Attribute> attributes = new HashSet<>(4);
-        attributes.add(mockAttribute(Long.class, "id"));
-        attributes.add(mockAttribute(String.class, "username"));
-        attributes.add(mockAttribute(String.class, "password"));
-        return attributes;
-    }
-
-    private Attribute mockAttribute(Class javaType, String name) {
-        Attribute attribute = mock(Attribute.class);
-        when(attribute.getJavaType()).thenReturn(javaType);
-        when(attribute.getName()).thenReturn(name);
-        return attribute;
-    }
 }
