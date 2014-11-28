d9a0fe6d0caed717a1c4f15adbc6fc05b53ef17d
Lawrence as Developer <smartlitchi@gmail.com>
2014-11-28 20:43:57 +0800

    Add test for OpenJPAUtil(BE-8)

---
+++ work/test/src/io/betterlife/util/jpa/OpenJPAUtilTest.java
@@ -0,0 +1,44 @@
+package io.betterlife.util.jpa;
+
+import org.apache.openjpa.persistence.OpenJPAQuery;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class OpenJPAUtilTest {
+
+    @Test
+    public void testGetInstance() throws Exception {
+        assertNotNull(OpenJPAUtil.getInstance());
+    }
+
+    @Test
+    public void testGetOpenJPAQuery() throws Exception {
+        EntityManager entityManager = mock(EntityManager.class);
+        OpenJPAQuery query = mock(OpenJPAQuery.class);
+        final String queryName = "User.getAll";
+        when(entityManager.createNamedQuery(queryName)).thenReturn(query);
+        assertEquals(query, OpenJPAUtil.getInstance().getOpenJPAQuery(entityManager, queryName));
+    }
+
+    @Test
+    public void testGetSingleResult() throws Exception {
+        internalGetSingleResultTest("xxxxx");
+    }
+
+    @Test
+    public void testGetSingleResultInt() throws Exception {
+        internalGetSingleResultTest(10);
+    }
+
+    private <T> void internalGetSingleResultTest(T expect) {
+        OpenJPAQuery query = mock(OpenJPAQuery.class);
+        when(query.getSingleResult()).thenReturn(expect);
+        T result = OpenJPAUtil.getInstance().getSingleResult(query);
+        assertEquals(expect, result);
+    }
+}
