5a9c19a45032502a73d85d5d8a62c1188cde9c7a
Lawrence as Developer <smartlitchi@gmail.com>
2014-12-04 02:48:20 +0800

    Fix bower file not downloaded successfully

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ git submodule sync
 git submodule update --init
 ln -s .builder/build.xml build.xml
 npm install -g bower
+cd work/WebContent
 bower install
 ant build.war junit.run.all findbugs simian pmd
