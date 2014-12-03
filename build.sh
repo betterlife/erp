3f6e172c98a6bfc00498b7c4fefb20e3d2551283
Lawrence as Developer <smartlitchi@gmail.com>
2014-12-04 03:03:16 +0800

    Use https not git to avoid timeout when bower install

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 git submodule sync
 git submodule update --init
+git config url."https://".insteadOf git://
 ln -s .builder/build.xml build.xml
 npm install -g bower
 cd work/WebContent
 bower install
-ant build.war junit.run.all findbugs simian pmd
+ant build.war junit.run.all 
