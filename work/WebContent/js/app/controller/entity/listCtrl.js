47f4d4791d18d751bc7e38e20791ce0bff92132c
Lawrence as Developer <smartlitchi@gmail.com>
2014-12-09 21:31:35 +0800

    Change the comment as this one contains more information.

--- work/WebContent/js/app/controller/entity/listCtrl.js
+++ work/WebContent/js/app/controller/entity/listCtrl.js
@@ -1,24 +1,20 @@
 /**
  *         enableSorting: true,
  columnDefs: [
-             {"field":"id","name":"Id"},
-             {"field":"lastModifyDate","name":"LastModifyDate"},
-             {"field":"lastModify","name":"LastModify"},
-             {"field":"createDate","name":"CreateDate"},
-             {"field":"name","name":"Name"},
-             {"field":"creator","name":"Creator"}
-         ],
-         data: [
-             {"name":null,"id":1,"lastModifyDate":1417839853680,"lastModify":null,"createDate":1417839853680,"creator":null},
-             {"name":null,"id":2,"lastModifyDate":1417840072000,"lastModify":null,"createDate":1417840072000,"creator":null},
-             {"name":null,"id":3,"lastModifyDate":1417840272780,"lastModify":null,"createDate":1417840272780,"creator":null},
-             {"name":null,"id":4,"lastModifyDate":1417840302970,"lastModify":null,"createDate":1417840302970,"creator":null},
-             {"name":null,"id":5,"lastModifyDate":1417840345540,"lastModify":null,"createDate":1417840345540,"creator":null},
-             {"name":null,"id":6,"lastModifyDate":1417840487580,"lastModify":null,"createDate":1417840487580,"creator":null},
-             {"name":null,"id":7,"lastModifyDate":1417840699440,"lastModify":null,"createDate":1417840699440,"creator":null},
-             {"name":null,"id":8,"lastModifyDate":1417868585900,"lastModify":null,"createDate":1417868585900,"creator":null},
-             {"name":"市场部","id":9,"lastModifyDate":1417869186200,"lastModify":null,"createDate":1417869186200,"creator":null}
-         ]
+            {name: 'firstName', field: 'first-name'},
+            {name: '1stFriend', field: 'friends[0]'},
+            {name: 'city', field: 'address.city'},
+            {name: 'getZip', field: 'getZip()', enableCellEdit: false}
+        ],
+        data: [{
+            "first-name": "Cox",
+            "friends": ["friend0"],
+            "address": {street: "301 Dove Ave", city: "Laurel", zip: "39565"},
+            "getZip": function () {
+                return this.address.zip;
+            }
+        }
+        ]
  */
 var listCtrl = function ($scope, $http, $location, loginService, $routeParams) {
     "use strict";
