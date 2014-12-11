a90bad6379b18029af12edc8e012596768512c62
Lawrence as Developer <smartlitchi@gmail.com>
2014-12-11 23:02:39 +0800

    Change code according to sonar report

--- work/src/io/betterlife/util/converter/ConverterFactory.java
+++ work/src/io/betterlife/util/converter/ConverterFactory.java
@@ -11,7 +11,7 @@ import java.util.Map;
  */
 public class ConverterFactory {
     private static ConverterFactory instance = new ConverterFactory();
-    private static Map<String, Converter> converters;
+    private Map<String, Converter> converters;
 
     private ConverterFactory() {
         converters = new HashMap<>();
