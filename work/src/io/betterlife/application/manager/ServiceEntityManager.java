92ecc3d3fb489f1f60022bc25fa58eab14b5de92
Lawrence as Developer <smartlitchi@gmail.com>
2014-12-17 18:47:17 +0800

    Add test for ServiceEntityManager

---
+++ work/src/io/betterlife/application/manager/ServiceEntityManager.java
@@ -0,0 +1,62 @@
+package io.betterlife.application.manager;
+
+import io.betterlife.application.config.ApplicationConfig;
+import io.betterlife.domains.BaseObject;
+import io.betterlife.domains.financial.CostCenter;
+import io.betterlife.domains.financial.Fund;
+import io.betterlife.domains.financial.FundCategory;
+import io.betterlife.domains.security.User;
+import io.betterlife.util.BLStringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author: Lawrence Liu(lawrence@betterlife.io)
+ * Date: 11/22/14
+ */
+public class ServiceEntityManager {
+    private boolean serviceEntityRegistered = false;
+    private final Map<String, Class<? extends BaseObject>> classes = new HashMap<>();
+    private static ServiceEntityManager instance = new ServiceEntityManager();
+
+    public static ServiceEntityManager getInstance() {
+        return instance;
+    }
+
+    private ServiceEntityManager() {}
+
+    public Class<? extends BaseObject> getServiceEntityClass(String name) {
+        if (!serviceEntityRegistered) {
+            registerEntities();
+            serviceEntityRegistered = true;
+        }
+        return classes.get(name);
+    }
+
+    public synchronized void registerServiceEntity(String name, Class<? extends BaseObject> clazz) {
+        classes.put(name, clazz);
+    }
+
+    public BaseObject entityObjectFromType(String objectType) throws InstantiationException, IllegalAccessException {
+        Class<? extends BaseObject> clazz = getServiceEntityClass(objectType);
+        return clazz.newInstance();
+    }
+
+    public Map<String, Class> getMetaFromEntityType(String entityType) {
+        final Class<? extends BaseObject> entityClass = getServiceEntityClass(entityType);
+        return MetaDataManager.getInstance().getMetaDataOfClass(entityClass);
+    }
+
+    /** Register all entities, this registry will be used for
+     *  Generate Entity CRUD rest services
+     *  Generate meta data for creation of front end CRUD UI.
+     */
+    public static void registerEntities(){
+        ServiceEntityManager.getInstance().registerServiceEntity("FundCategory", FundCategory.class);
+        ServiceEntityManager.getInstance().registerServiceEntity("User", User.class);
+        ServiceEntityManager.getInstance().registerServiceEntity("CostCenter", CostCenter.class);
+        ServiceEntityManager.getInstance().registerServiceEntity("Fund", Fund.class);
+    }
+
+}
