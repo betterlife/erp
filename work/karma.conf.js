6424dd911d8ccb1929568a6e27e1654e132fb374
Lawrence as Developer <smartlitchi@gmail.com>
2015-01-16 23:10:56 +0800

    Work on BE-72, Support javascript unit test

---
+++ work/karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function(config){
+    config.set({
+
+        basePath : '.',
+
+        files : [
+            'WebContent/js/bower_components/angular/angular.js',
+            'WebContent/js/bower_components/angular-route/angular-route.js',
+            'WebContent/js/bower_components/angular-mocks/angular-mocks.js',
+            'WebContent/js/bower_components/angular-cookies/angular-cookies.js',
+            'WebContent/js/bower_components/angular-bootstrap/ui-bootstrap.js',
+            'WebContent/js/bower_components/angular-ui-grid/ui-grid.js',
+            'WebContent/js/app/**/*.js',
+            'test/js/app/**/*.js'
+        ],
+
+        autoWatch : true,
+        frameworks: ['jasmine'],
+        browsers : ['Chrome'],
+
+        plugins : [
+            'karma-chrome-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+        ],
+
+        junitReporter : {
+            outputFile: '../target/data/junit/js.xml',
+            suite: 'unit'
+        }
+
+    });
+};
---
+++ work/test/js/app/controller/loginCtrlSpec.js
@@ -0,0 +1,39 @@
+describe("Unit: Testing Login Controllers", function() {
+
+  var $controller, loginCtrl;
+
+  beforeEach(module('mainApp'));
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+
+  describe('Login Controller Specs', function() {
+    it('Should have a login controller', function() {
+      var $scope = {};
+      var controller = $controller('loginCtrl', { $scope: $scope });
+      expect(controller).not.toBe(undefined);
+      expect(controller).not.toBe(null);
+    });
+
+    it('Should return error msg when username and password are undefined', function () {
+      var $scope = {
+        username : undefined,
+        password : undefined
+      };
+      $controller('loginCtrl', { $scope: $scope });
+      $scope.login();
+      expect($scope.login_errors.length).toBe(1);
+      expect($scope.login_errors).toContain('请输入用户名密码');
+    });
+
+    it('Should have a logout controller', function() {
+      var $scope = {};
+      var controller = $controller('logoutCtrl', { $scope: $scope });
+      expect(controller).not.toBe(undefined);
+      expect(controller).not.toBe(null);
+    });
+  });
+
+});
