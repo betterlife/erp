f825764d2dad446950e2447d94b438e72baa5814
Lawrence <lawrence@betterlife.io>
2015-01-18 21:31:04 +0800

    Finishes BE-60(Add a modal confirm window when delete records).

--- work/WebContent/js/app/controller/entity/listCtrl.js
+++ work/WebContent/js/app/controller/entity/listCtrl.js
@@ -16,7 +16,7 @@
         }
         ]
  */
-var listCtrl = function ($scope, $http, $location, loginService, i18nService, $routeParams) {
+var listCtrl = function ($scope, $http, $location, $modal, loginService, i18nService, $routeParams) {
     "use strict";
     i18nService.setCurrentLang('zh-cn');
     $scope.entityType = $routeParams.entityType;
@@ -45,22 +45,46 @@ var listCtrl = function ($scope, $http, $location, loginService, i18nService, $r
 
     $scope.myViewModel = {
         deActive: function (row) {
-            var id = row.entity.id;
-            var entity = {
-                'id': id,
-                'active': false
-            };
-            var index = $scope.gridOptions.data.indexOf(row.entity);
-            $http.put("/rest/" + $scope.captalizedEntityType  + "/" + id, {
-                'entity': entity
-            }, {}).success(function (data) {
-                $scope.gridOptions.data.splice(index, 1);
-            }).error(function (data, status) {
+            var modalTitle;
+            if (row.entity.name) {
+                modalTitle = "确认删除名为 " + row.entity.name + " 的 " + $scope.captalizedEntityType + " 数据？"
+            } else {
+                modalTitle = "确认删除编号为 " + row.entity.id + " 的 " + $scope.captalizedEntityType + " 数据？"
+            }
+            var modalInstance = $modal.open({
+                templateUrl: '/templates/modal.html',
+                controller: modalInstanceCtrl,
+                resolve: {
+                    modalData : function () {
+                        return {
+                            title     : modalTitle,
+                            body      : "数据将会被从用户界面上删除，并不再包含于相关报表中。",
+                            yesLabel  : "确定",
+                            noLabel   : "取消"
+                        };
+                    }
+                }
+            });
+            modalInstance.result.then(function () {
+                var id = row.entity.id;
+                var entity = {
+                    'id': id, 'active': false
+                };
+                var index = $scope.gridOptions.data.indexOf(row.entity);
+                $http.put("/rest/" + $scope.captalizedEntityType  + "/" + id, {
+                    'entity': entity
+                }, {}).success(function () {
+                    $scope.gridOptions.data.splice(index, 1);
+                }).error(function () {
+                    console.info('Error to delete data:[' + row + ']');
+                });
+            }, function () {
+                console.debug('Modal dismissed at: ' + new Date());
             });
         }
     };
 };
 
-listCtrl.$inject = ['$scope', '$http', '$location', 'loginService', 'i18nService', '$routeParams'];
+listCtrl.$inject = ['$scope', '$http', '$location', '$modal', 'loginService', 'i18nService', '$routeParams'];
 
 angular.module('mainApp').controller('listCtrl', listCtrl);
