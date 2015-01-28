6515c092d4d24bdf6b9f886808dd741b8690dad1
Lawrence <lawrence@betterlife.io>
2015-01-28 12:59:09 +0800

    Work on BE-74 Implement purchase order management, add purchase order entity and unit test.

--- work/src/io/betterlife/domains/order/PurchaseOrder.java
+++ work/src/io/betterlife/domains/order/PurchaseOrder.java
@@ -1,10 +1,15 @@
 package io.betterlife.domains.order;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.betterlife.domains.BaseObject;
+import io.betterlife.domains.catalog.Product;
+import io.betterlife.domains.common.Supplier;
+import io.betterlife.domains.security.User;
+import io.betterlife.rest.Form;
 
-import javax.persistence.Entity;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * Author: Lawrence Liu
@@ -13,7 +18,101 @@ import javax.persistence.NamedQuery;
 @Entity
 @NamedQueries({
     @NamedQuery(name = "PurchaseOrder.getById", query = "SELECT c FROM PurchaseOrder c WHERE c.id = :id AND c.active = TRUE"),
-    @NamedQuery(name = "PurchaseOrder.getAll",  query = "SELECT c FROM PurchaseOrder c WHERE c.active = TRUE")
+    @NamedQuery(name = "PurchaseOrder.getAll", query = "SELECT c FROM PurchaseOrder c WHERE c.active = TRUE")
 })
 public class PurchaseOrder extends BaseObject {
+
+    @ManyToOne
+    @Form(DisplayRank = 5, RepresentField = "name")
+    public Product getProduct() {
+        return getValue("product");
+    }
+
+    public void setProduct(Product product) {
+        setValue("product", product);
+    }
+
+    @Transient
+    @Form(DisplayRank = 10, RepresentField = "name")
+    public Supplier getSupplier() {
+        return getProduct().getSupplier();
+    }
+
+    @Form(DisplayRank = 15, RepresentField = "name")
+    public BigDecimal getPricePerUnit() {
+        return getValue("pricePerUnit");
+    }
+
+    public void setPricePerUnit(BigDecimal price) {
+        setValue("pricePerUnit", price);
+    }
+
+    @Form(DisplayRank = 20, RepresentField = "name")
+    public BigDecimal getQuantity() {
+        return getValue("quantity");
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        setValue("quantity", quantity);
+    }
+
+    @Transient
+    @Form(DisplayRank = 25)
+    public BigDecimal getAmount() {
+        return getQuantity().multiply(getPricePerUnit());
+    }
+
+    @Temporal(value = TemporalType.DATE)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd", timezone = "CST")
+    @Form(DisplayRank = 30)
+    public Date getPurchaseDate() {
+        return getValue("purchaseDate");
+    }
+
+    public void setPurchaseDate(Date date) {
+        setValue("purchaseDate", date);
+    }
+
+    @Form(DisplayRank = 30)
+    public BigDecimal getLogisticAmount() {
+        return getValue("logisticAmount");
+    }
+
+    public void setLogisticAmount(BigDecimal amount) {
+        setValue("logisticAmount", amount);
+    }
+
+    @Form(DisplayRank = 35)
+    @Transient
+    public BigDecimal getUnitLogisticAmount() {
+        return getLogisticAmount().divide(getQuantity(), 2, BigDecimal.ROUND_HALF_UP);
+    }
+
+    @Form(DisplayRank = 40)
+    public BigDecimal getOtherAmount() {
+        return getValue("otherAmount");
+    }
+
+    public void setOtherAmount(BigDecimal otherAmount) {
+        setValue("otherAmount", otherAmount);
+    }
+
+    @ManyToOne
+    @Form(DisplayRank = 45, RepresentField = "displayName")
+    public User getUser() {
+        return getValue("user");
+    }
+
+    public void setUser(User user) {
+        setValue("user", user);
+    }
+
+    public void setRemark(String remark) {
+        setValue("remark", remark);
+    }
+
+    @Form(DisplayRank = 50)
+    public String getRemark() {
+        return getValue("remark");
+    }
 }
---
+++ work/test/src/io/betterlife/domains/order/PurchaseOrderTest.java
@@ -0,0 +1,217 @@
+package io.betterlife.domains.order;
+
+import io.betterlife.domains.catalog.Product;
+import io.betterlife.domains.common.Supplier;
+import io.betterlife.domains.security.User;
+import io.betterlife.util.EntityVerifyUtil;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+import javax.persistence.Entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class PurchaseOrderTest {
+
+    PurchaseOrder purchaseOrder;
+
+    @Before
+    public void setUp() throws Exception {
+        purchaseOrder = new PurchaseOrder();
+    }
+
+    @Test
+    public void testProduct() throws Exception {
+        Product product = Mockito.mock(Product.class);
+        purchaseOrder.setProduct(product);
+        assertNotNull(purchaseOrder.getProduct());
+        assertSame(product, purchaseOrder.getProduct());
+        Product product1 = Mockito.mock(Product.class);
+        purchaseOrder.setProduct(product1);
+        assertNotNull(purchaseOrder.getProduct());
+        assertNotEquals(product, purchaseOrder.getProduct());
+        assertSame(product1, purchaseOrder.getProduct());
+    }
+
+    @Test
+    public void testGetSupplier() throws Exception {
+        Product product = Mockito.mock(Product.class);
+        Supplier supplier = Mockito.mock(Supplier.class);
+        when(product.getSupplier()).thenReturn(supplier);
+        purchaseOrder.setProduct(product);
+        assertNotNull(purchaseOrder.getSupplier());
+        assertSame(supplier, purchaseOrder.getSupplier());
+        Product newProduct = Mockito.mock(Product.class);
+        Supplier newSupplier = Mockito.mock(Supplier.class);
+        when(newProduct.getSupplier()).thenReturn(newSupplier);
+        purchaseOrder.setProduct(newProduct);
+        assertNotNull(purchaseOrder.getSupplier());
+        assertNotEquals(supplier, purchaseOrder.getSupplier());
+        assertSame(newSupplier, purchaseOrder.getSupplier());
+    }
+
+    @Test
+    public void testPricePerUnit() throws Exception {
+        BigDecimal ppu = new BigDecimal("20.00");
+        purchaseOrder.setPricePerUnit(ppu);
+        assertNotNull(purchaseOrder.getPricePerUnit());
+        assertSame(ppu, purchaseOrder.getPricePerUnit());
+        BigDecimal newPPU = new BigDecimal("100.21");
+        purchaseOrder.setPricePerUnit(newPPU);
+        assertNotNull(purchaseOrder.getPricePerUnit());
+        assertNotEquals(ppu, purchaseOrder.getPricePerUnit());
+        assertSame(newPPU, purchaseOrder.getPricePerUnit());
+    }
+
+    @Test
+    public void testQuantity() throws Exception {
+        BigDecimal qty = new BigDecimal("20.00");
+        purchaseOrder.setQuantity(qty);
+        assertNotNull(purchaseOrder.getQuantity());
+        assertSame(qty, purchaseOrder.getQuantity());
+        BigDecimal newQty = new BigDecimal("100.00");
+        purchaseOrder.setQuantity(newQty);
+        assertNotNull(purchaseOrder.getQuantity());
+        assertNotEquals(qty, purchaseOrder.getQuantity());
+        assertSame(newQty, purchaseOrder.getQuantity());
+    }
+
+    @Test
+    public void testAmount() throws Exception {
+        BigDecimal qty = new BigDecimal("20.00");
+        purchaseOrder.setQuantity(qty);
+        BigDecimal ppu = new BigDecimal("50.00");
+        purchaseOrder.setPricePerUnit(ppu);
+        BigDecimal amount = purchaseOrder.getAmount();
+        assertNotNull(amount);
+        assertEquals(qty.multiply(ppu), amount);
+        BigDecimal newQty = new BigDecimal("21");
+        purchaseOrder.setQuantity(newQty);
+        BigDecimal newAmt = purchaseOrder.getAmount();
+        assertNotNull(newAmt);
+        assertNotEquals(qty.multiply(ppu), newAmt);
+        assertEquals(newQty.multiply(ppu), newAmt);
+        BigDecimal newPpu = new BigDecimal("45.00");
+        purchaseOrder.setPricePerUnit(newPpu);
+        BigDecimal newAmt2 = purchaseOrder.getAmount();
+        assertNotNull(newAmt2);
+        assertNotEquals(qty.multiply(ppu), newAmt2);
+        assertNotEquals(newQty.multiply(ppu), newAmt2);
+        assertEquals(newQty.multiply(newPpu), newAmt2);
+    }
+
+    @Test
+    public void testPurchaseDate() throws Exception {
+        Date date = new Date();
+        purchaseOrder.setPurchaseDate(date);
+        assertNotNull(purchaseOrder.getPurchaseDate());
+        assertEquals(date, purchaseOrder.getPurchaseDate());
+        Date date1 = new Date();
+        purchaseOrder.setPurchaseDate(date1);
+        final Date date2 = purchaseOrder.getPurchaseDate();
+        assertNotNull(date2);
+        assertNotSame(date, date2);
+        assertSame(date1, date2);
+    }
+
+    @Test
+    public void testGetLogisticAmount() throws Exception {
+        BigDecimal bd = new BigDecimal("10.05");
+        purchaseOrder.setLogisticAmount(bd);
+        assertNotNull(purchaseOrder.getLogisticAmount());
+        assertEquals(bd, purchaseOrder.getLogisticAmount());
+        BigDecimal bd1 = new BigDecimal("20.90");
+        purchaseOrder.setLogisticAmount(bd1);
+        final BigDecimal amount = purchaseOrder.getLogisticAmount();
+        assertNotNull(amount);
+        assertNotEquals(bd, amount);
+        assertSame(bd1, amount);
+    }
+
+    @Test
+    public void testGetUnitLogisticAmount() throws Exception {
+        BigDecimal totalLogisticAmt = new BigDecimal("8");
+        purchaseOrder.setLogisticAmount(totalLogisticAmt);
+        BigDecimal qty = new BigDecimal("10");
+        purchaseOrder.setQuantity(qty);
+        BigDecimal unitLogisticAmt = purchaseOrder.getUnitLogisticAmount();
+        assertNotNull(unitLogisticAmt);
+        assertEquals(totalLogisticAmt.divide(qty, 2, BigDecimal.ROUND_HALF_UP), unitLogisticAmt);
+        BigDecimal newTotalLogisticAmt = new BigDecimal("10");
+        purchaseOrder.setLogisticAmount(newTotalLogisticAmt);
+        unitLogisticAmt = purchaseOrder.getUnitLogisticAmount();
+        assertNotNull(unitLogisticAmt);
+        assertNotEquals(totalLogisticAmt.divide(qty, 2, BigDecimal.ROUND_HALF_UP), unitLogisticAmt);
+        assertEquals(newTotalLogisticAmt.divide(qty, 2, BigDecimal.ROUND_HALF_UP), unitLogisticAmt);
+        BigDecimal newQty = new BigDecimal("5");
+        purchaseOrder.setQuantity(newQty);
+        unitLogisticAmt = purchaseOrder.getUnitLogisticAmount();
+        assertNotNull(unitLogisticAmt);
+        assertNotEquals(totalLogisticAmt.divide(qty, 2, BigDecimal.ROUND_HALF_UP), unitLogisticAmt);
+        assertNotEquals(newTotalLogisticAmt.divide(qty, 2, BigDecimal.ROUND_HALF_UP), unitLogisticAmt);
+        assertEquals(newTotalLogisticAmt.divide(newQty, 2, BigDecimal.ROUND_HALF_UP), unitLogisticAmt);
+    }
+
+    @Test
+    public void testOtherAmount() throws Exception {
+        BigDecimal bd = new BigDecimal("10.05");
+        purchaseOrder.setOtherAmount(bd);
+        assertNotNull(purchaseOrder.getOtherAmount());
+        assertEquals(bd, purchaseOrder.getOtherAmount());
+        BigDecimal bd1 = new BigDecimal("20.90");
+        purchaseOrder.setOtherAmount(bd1);
+        final BigDecimal amount = purchaseOrder.getOtherAmount();
+        assertNotNull(amount);
+        assertNotEquals(bd, amount);
+        assertSame(bd1, amount);
+    }
+
+    @Test
+    public void testUser() throws Exception {
+        User user = mock(User.class);
+        purchaseOrder.setUser(user);
+        User user1 = mock(User.class);
+        assertNotNull(purchaseOrder.getUser());
+        assertSame(user, purchaseOrder.getUser());
+        purchaseOrder.setUser(user1);
+        assertNotNull(purchaseOrder.getUser());
+        assertNotEquals(user, purchaseOrder.getUser());
+        assertSame(user1, purchaseOrder.getUser());
+    }
+
+    @Test
+    public void testRemark() throws Exception {
+        String str = "I am a remark with 中文";
+        purchaseOrder.setRemark(str);
+        assertNotNull(purchaseOrder.getRemark());
+        assertEquals(str, purchaseOrder.getRemark());
+        String str1 = "I am a 新的 remark with 中文";
+        purchaseOrder.setRemark(str1);
+        final String remark = purchaseOrder.getRemark();
+        assertNotNull(remark);
+        assertNotEquals(str, remark);
+        assertSame(str1, remark);
+    }
+
+    @Test
+    public void testEntityAnnotationDefined() {
+        Entity entity = PurchaseOrder.class.getAnnotation(Entity.class);
+        assertNotNull(entity);
+    }
+
+    @Test
+    public void testGetAllQueryDefined() {
+        EntityVerifyUtil.getInstance().verifyQueryDefined(PurchaseOrder.class, PurchaseOrder.class.getSimpleName() + "." + "getAll");
+    }
+
+    @Test
+    public void testGetByIdQueryDefined() {
+        EntityVerifyUtil.getInstance().verifyQueryDefined(PurchaseOrder.class, PurchaseOrder.class.getSimpleName() + "." + "getById");
+    }
+}
