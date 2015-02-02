63776b70f899f927124d3ff2b11605ea33e77b61
Lawrence <lawrence@betterlife.io>
2015-02-02 16:44:08 +0800

    Finishes BE-60(Add a modal confirm window when delete records).

---
+++ work/src/io/betterlife/util/condition/ProductCodeEditableCondition.java
@@ -0,0 +1,17 @@
+package io.betterlife.util.condition;
+
+import io.betterlife.application.manager.FieldMeta;
+import io.betterlife.domains.BaseObject;
+
+/**
+ * Author: Lawrence Liu
+ * Date: 2/2/15
+ */
+public class ProductCodeEditableCondition implements Condition {
+    @Override
+    public boolean evaluate(String entityType, FieldMeta fieldMeta, BaseObject baseObject, String operationType) {
+        return "Create".equals(operationType) &&
+            "product".equals(entityType) &&
+            "code".equals(fieldMeta.getName());
+    }
+}
