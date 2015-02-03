dc422bd820f3f027d8249373ec805b7952cc64be
Lawrence <lawrence@betterlife.io>
2015-02-03 17:37:10 +0800

    Adjust the build info format.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 git submodule sync
 git submodule update --init
 git config url."https://".insteadOf git://
@@ -8,7 +8,7 @@ cd work/WebContent
 bower install
 cd ../../
 ant build.flat
-echo $CI_BUILD_NUMBER @ $CI_BUILD_URL \n $CI_MESSAGE> target/artifact/flat/build.txt
+printf "Build Number:\t$CI_BUILD_NUMBER @ $CI_BUILD_URL \nCommit Info :\t $CI_MESSAGE\n"> target/artifact/flat/build.txt
 ant build.war junit.run.all
 cd work
 npm install
