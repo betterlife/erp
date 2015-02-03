4c8776de066012c78a034e0973f8e66f467ba998
Lawrence <lawrence@betterlife.io>
2015-02-03 17:19:47 +0800

    Add build number and build URL to build.txt for bug tracking.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 git submodule sync
 git submodule update --init
 git config url."https://".insteadOf git://
@@ -7,6 +7,8 @@ ln -s .builder/build.xml build.xml
 cd work/WebContent
 bower install
 cd ../../
+ant build.flat
+echo $CI_BUILD_NUMBER @ $CI_BUILD_URL \n $CI_MESSAGE> target/artifact/flat/build.txt
 ant build.war junit.run.all
 cd work
 npm install
