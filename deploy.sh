d6d6ac3e079b34a74d6ce6751f4eb98ba0ba556a
Lawrence <lawrence@betterlife.io>
2015-02-04 11:32:11 +0800

    Add development and production deployment support.

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-if ["$CI_BRANCH" = "develop"]
+if [ "$CI_BRANCH" = "develop" ]
   then
-    $USER = $OPEN_SHIFT_DEV_USER
-    $HOST = $OPEN_SHIFT_DEV_HOST
+    DEPLOY_USER=$OPEN_SHIFT_DEV_USER
+    DEPLOY_HOST=$OPEN_SHIFT_DEV_HOST
 elif [ "$CI_BRANCH" = "master" ]
   then
-    $USER = $OPEN_SHIFT_PRD_USER
-    $HOST = $OPEN_SHIFT_PRD_HOST
+    DEPLOY_USER=$OPEN_SHIFT_PRD_USER
+    DEPLOY_HOST=$OPEN_SHIFT_PRD_HOST
 fi
 
 cp target/artifact/package/betterlife-erp_0.3.war target/artifact/package/ROOT.war
-scp target/artifact/package/ROOT.war $USER@$HOST:app-root/dependencies/jbossews/webapps
+scp target/artifact/package/ROOT.war $DEPLOY_USER@$DEPLOY_HOST:app-root/dependencies/jbossews/webapps
