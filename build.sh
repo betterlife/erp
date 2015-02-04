dd6bb6b1192c2adb93b4ed365f4e4378db86b18d
Lawrence <lawrence@betterlife.io>
2015-02-04 11:42:13 +0800

    Add branch name display in the build info file.

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cd work/WebContent
 bower install
 cd ../../
 ant build.flat
-printf "Build Number:\t$CI_BUILD_NUMBER @ $CI_BUILD_URL \nCommit Info :\t$CI_MESSAGE\n"> target/artifact/flat/build.txt
+printf "Branch  Name:\t$CI_BRANCH\n Build Number:\t$CI_BUILD_NUMBER @ $CI_BUILD_URL \nCommit Info :\t$CI_MESSAGE\n"> target/artifact/flat/build.txt
 ant build.war junit.run.all
 cd work
 npm install
