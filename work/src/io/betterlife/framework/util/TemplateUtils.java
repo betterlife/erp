betterlife/erp

commit de6d4e5811521d16a722e0329bccc96d7896b45e

Add placeholder to the type head controller.

work/src/io/betterlife/framework/util/TemplateUtils.java
@@ -10,6 +10,7 @@ import io.betterlife.framework.persistence.NamedQueryRules;
 import io.betterlife.framework.condition.Evaluator;
 import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.reflect.MethodUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,6 +19,7 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Query;
 import javax.servlet.ServletContext;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -125,33 +127,55 @@ public class TemplateUtils {
             .replaceAll("\\$options", sb.toString());
     }
 
-    public String getBaseObjectController(ServletContext context, FieldMeta fieldMeta, Class<? extends BaseObject> clazz) {
+    public String getBaseObjectController(ServletContext context, FieldMeta fieldMeta,
+                                          Class<? extends BaseObject> clazz) {
         long number = BaseOperator.getInstance().getObjectCount(clazz);
         String result = "";
-        if (number <= ApplicationConfig.MaxNumberOfObjectForSelectController) {
-            List<BaseObject> objects = BaseOperator.getInstance().getBaseObjects(
-                NamedQueryRules.getInstance().getAllQueryForEntity(clazz.getSimpleName()), null
-            );
-            StringBuilder sb = new StringBuilder();
-            for (BaseObject baseObject : objects) {
-                sb.append(String.format("%n\t<option value='%s'>%s</option>", baseObject.getId(),
-                                        baseObject.getValue(fieldMeta.getRepresentField())));
-            }
-            sb.append("\n");
-            String template = getHtmlTemplate(context, "templates/fields/baseobject.select.tpl.html");
-            result = template
-                .replaceAll("\\$name", fieldMeta.getName())
-                .replaceAll("\\$ngModel", EntityUtils.getInstance().getNgModelNameForField(fieldMeta.getName()) + ".id")
-                .replaceAll("\\$options", sb.toString());
-        } else {
-            String template = getHtmlTemplate(context, "templates/fields/baseobject.typehead.tpl.html");
-            final String ngModelField = EntityUtils.getInstance().getNgModelNameForField(fieldMeta.getName());
-            result = template
-                .replaceAll("\\$name", fieldMeta.getName())
-                .replaceAll("\\$ngModel", ngModelField)
-                .replaceAll("\\$entityType", BLStringUtils.uncapitalize(clazz.getSimpleName()))
-                .replaceAll("\\$representField", fieldMeta.getRepresentField());
+        if (number <= ApplicationConfig.MaxNumberOfObjectForSelectController)
+            result = getBaseObjectSelectController(context, fieldMeta, clazz);
+        else {
+            result = getBaseObjectTypeHeadController(context, fieldMeta, clazz);
         }
+        return result;
+    }
+
+    public String getBaseObjectTypeHeadController(ServletContext context, FieldMeta fieldMeta,
+                                                  Class<? extends BaseObject> clazz) {
+        String result;
+        String template = getHtmlTemplate(context, "templates/fields/baseobject.typehead.tpl.html");
+        final String ngModelField = EntityUtils.getInstance().getNgModelNameForField(fieldMeta.getName());
+        result = template
+            .replaceAll("\\$name", fieldMeta.getName())
+            .replaceAll("\\$ngModel", ngModelField)
+            .replaceAll("\\$placeholder", I18n.getInstance().get(fieldMeta.getName(), ApplicationConfig.getLocale()))
+            .replaceAll("\\$entityType", BLStringUtils.uncapitalize(clazz.getSimpleName()))
+            .replaceAll("\\$representField", fieldMeta.getRepresentField());
+        return result;
+    }
+
+    public String getBaseObjectSelectController(ServletContext context, FieldMeta fieldMeta,
+                                                Class<? extends BaseObject> clazz) {
+        String result;List<BaseObject> objects = BaseOperator.getInstance().getBaseObjects(
+            NamedQueryRules.getInstance().getAllQueryForEntity(clazz.getSimpleName()), null
+        );
+        StringBuilder sb = new StringBuilder();
+        final String representField = fieldMeta.getRepresentField();
+        final String methodName = "get" + BLStringUtils.capitalize(representField);
+        for (BaseObject baseObject : objects) {
+            try {
+                final Object value = MethodUtils.invokeExactMethod(baseObject, methodName);
+                sb.append(String.format("%n\t<option value='%s'>%s</option>", baseObject.getId(), value));
+            } catch (Exception e) {
+                logger.error("Failed to get field[%s] of Object[%s] via method[%s]",
+                             representField, baseObject, methodName);
+            }
+        }
+        sb.append("\n");
+        String template = getHtmlTemplate(context, "templates/fields/baseobject.select.tpl.html");
+        result = template
+            .replaceAll("\\$name", fieldMeta.getName())
+            .replaceAll("\\$ngModel", EntityUtils.getInstance().getNgModelNameForField(fieldMeta.getName()) + ".id")
+            .replaceAll("\\$options", sb.toString());
         return result;
     }
 

work/WebContent/templates/fields/baseobject.typehead.tpl.html
@@ -3,5 +3,6 @@
 <input type="hidden" ng-model="$ngModel.id" name="$name">
 <input type="text" typeahead="baseObject.description for baseObject in getBaseObjects('$entityType', '$representField', $viewValue)"
        typeahead-on-select="onTypeHeadSelect($item, $model, $label, '$name')"
+       placeholder="$placeholder (输入关键字搜索)"
        ng-model="$ngModel.$representField" typeahead-loading="loading$name" class="form-control">
 <span ng-show="loading$name" class="glyphicon glyphicon-refresh"></span>

work/src/io/betterlife/framework/persistence/BaseOperator.java
@@ -136,7 +136,8 @@ public class BaseOperator {
 
     public long getObjectCount(Class<? extends BaseObject> clazz) {
         final String tableName = clazz.getSimpleName().equals("User")? "UserEntity" : clazz.getSimpleName();
-        Query query = getEntityManager().createQuery("SELECT COUNT(p.id) from " + tableName + " p", clazz);
+        Query query = getEntityManager().createQuery("SELECT COUNT(p.id) from " + tableName + " p where p.active = :active", clazz);
+        query.setParameter("active", true);
         return getSingleResult(query);
     }
 }

commit 514422fbbd9447b72c4471b9a7ddb7c7b147eed1

Fix test failure introduced by de6d4e58.

work/src/io/betterlife/framework/util/TemplateUtils.java
@@ -163,7 +163,10 @@ public class TemplateUtils {
         final String methodName = "get" + BLStringUtils.capitalize(representField);
         for (BaseObject baseObject : objects) {
             try {
-                final Object value = MethodUtils.invokeExactMethod(baseObject, methodName);
+                Object value = baseObject.getValue(representField);
+                if (null == value) {
+                    value = MethodUtils.invokeExactMethod(baseObject, methodName);
+                }
                 sb.append(String.format("%n\t<option value='%s'>%s</option>", baseObject.getId(), value));
             } catch (Exception e) {
                 logger.error("Failed to get field[%s] of Object[%s] via method[%s]",